a665b14ed99dbe737944d620c9393f2bc93c3d33
Ana Júlia <alvncar@gmail.com>
2024-03-01 15:38:17 -0300

    fix: hydration problem

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { ApplicationComponent } from '../application/application.component';
 import { ChallengeCardComponent } from '../challenge-card/challenge-card.component';
 import { ScheduleCardComponent } from '../schedule-card/schedule-card.component';
 import { ButtonComponent } from '../button/button.component';
 import { AwardsCardComponent } from '../awards-card/awards-card.component';
+import { ApplicationFormComponent } from '../application-form/application-form.component';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +16,7 @@ import { AwardsCardComponent } from '../awards-card/awards-card.component';
     ChallengeCardComponent,
     ScheduleCardComponent,
     AwardsCardComponent,
-    ApplicationComponent,
+    ApplicationFormComponent,
     ButtonComponent,
   ],
   providers: [],
@@ -24,7 +24,5 @@ import { AwardsCardComponent } from '../awards-card/awards-card.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  button_primary = {
-    styles: { backgroundColor: '#003262', color: '#FFFFFF', padding: '1.25rem 3rem', fontSize: '1.5rem' }
-  };
+
 }
