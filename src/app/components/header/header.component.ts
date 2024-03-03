91d774f03496e417d94e314beb9341be61a4c136
Ana Júlia <alvncar@gmail.com>
2024-03-02 21:17:53 -0300

    ui: header component responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    ButtonComponent
+    ButtonComponent,
+    NavbarComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
