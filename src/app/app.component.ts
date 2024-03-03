9eb85684021a43dc56dee16cacb33136add9bcc6
Ana Júlia <alvncar@gmail.com>
2024-03-03 16:10:16 -0300

    perf: meta tag added for SEO best practices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,11 +9,19 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [
     RouterOutlet,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule
   ],
+  providers: [ Meta ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'innova';
+export class AppComponent implements OnInit {
+  title = 'Innova';
+
+  constructor(private metaService:Meta) { }
+
+  ngOnInit() {
+    this.metaService.addTag( { name:'description',content:"Landing page desenvolvida com Angular 17 e Sass para o teste técnico de front-end da Innova - All the brand"});
+  }
 }
